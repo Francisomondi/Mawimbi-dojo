5d65f0f45ba0c057cfd490ce5246bc66f0b685f5
francis omondi <francisomondi17@gmail.com>
2022-07-15 00:09:34 +0300

    installationation of packagaes, and created our first object

--- app.js
+++ app.js
@@ -1,28 +1,3 @@
-const http = require("http")
+const person = require("./person");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === '/') {
-    res.setHeader("content-type", "text/html")
-    res.write("<html>")
-    res.write("<head><title>this is my heading</title></head>")
-    res.write("<body><form><input type='text'><button type='submit'>submit</button></form></body>")
-    res.write("</html>")
-    return res.end()
-
-
-  }
-  if (url === "/messages" && method === "POST") {
-    res.setHeader("content-type", "text/html")
-    res.write("<html>")
-    res.write("<head><title>secong heading</title></head>")
-    res.write("<body><h1>this is the main body</h1></body>")
-    res.write("</html>")
-    return res.end()
-
-  }
-
-});
-
-server.listen(3000)
+console.log(person);
---
+++ person.js
@@ -0,0 +1,5 @@
+const person = {
+    name: "francis Omondi",
+    age: 45
+};
+module.exports = person;
